307909164b7ec5ff62a6e5e693b2a5ca17a6e6d5
Thomas <tgtvete@ntebb.no>
2024-04-20 12:54:00 +0200

    klasse-leking

---
+++ canvas.js
@@ -0,0 +1,10 @@
+/**
+ * @type HTMLCanvasElement
+ */
+
+const canvas = document.getElementById("myHeader");
+const ctx = canvas.getContext("2d");
+
+ctx.fillStyle = "red";
+ctx.fillText("THOMAS", 100, 100)
+
